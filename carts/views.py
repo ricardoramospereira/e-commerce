acfec96feb6552a47e862e36762d03550051d28f
Ricardo Ramos Pereira <ricardoramospereira33@gmail.com>
2023-02-09 15:47:32 -0300

    cart_button

--- carts/views.py
+++ carts/views.py
@@ -1,5 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from store.models import Product
+from .models import Cart, CartItem
+
+from django.http import HttpResponse
 
 # Create your views here.
-def cart(request):
-    return render(request, 'store/cart.html')
+def _cart_id(request):
+    cart = request.session.session_key
+    if not cart:
+        cart = request.session.create()
+    return cart
+
+def add_cart(request, product_id):
+    product = Product.objects.get(id=product_id) #get the product
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request)) # using the cart_id in the session
+    except Cart.DoesNotExist:
+        cart = Cart.objects.create(
+            cart_id =_cart_id(request)
+        )
+    cart.save()
+
+    try:
+        cart_item = CartItem.objects.get(product=product, cart=cart)
+        cart_item.quantity += 1 # click on adicionar ao carrinho add plus one
+        cart_item.save()
+    except CartItem.DoesNotExist:
+        cart_item = CartItem.objects.create(
+            product = product,
+            quantity = 1,
+            cart = cart,
+        )
+        cart_item.save()
+    return redirect('cart')
+
+def cart(request, total=0, cart_items = None):
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+    except :
+        pass # ignore
+
+    context = {
+        'total': total,
+        'quantity': quantity,
+        'cart_items': cart_items,
+
+    }
+    return render(request, 'store/cart.html', context)
