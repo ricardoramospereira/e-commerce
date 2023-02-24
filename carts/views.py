9c8e533a416782f089ab04f611a4d8682511c652
Ricardo Ramos Pereira <ricardoramospereira33@gmail.com>
2023-02-24 19:40:11 -0300

    paginator

--- carts/views.py
+++ carts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from store.models import Product
 from .models import Cart, CartItem
+from django.core.exceptions import ObjectDoesNotExist
 
 from django.http import HttpResponse
 
@@ -54,6 +55,8 @@ def remove_cart_item(request, product_id):
 
 def cart(request, total=0, quantity=0, cart_items = None):
     try:
+        tax = 0
+        grand_total = 0
         cart = Cart.objects.get(cart_id=_cart_id(request))
         cart_items = CartItem.objects.filter(cart=cart, is_active=True)
         for cart_item in cart_items:
@@ -61,7 +64,7 @@ def cart(request, total=0, quantity=0, cart_items = None):
             quantity += cart_item.quantity
         tax = (2 * total) /100
         grand_total = total + tax
-    except :
+    except ObjectDoesNotExist:
         pass # ignore
 
     context = {
